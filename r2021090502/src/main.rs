a5ecc16284aae2f980ef5060f7b9098979a863e3
nmapcoco <nmapcoco@gmail.com>
2021-09-05 22:23:30 +0800

    ad

--- r2021090502/src/main.rs
+++ r2021090502/src/main.rs
@@ -20,5 +20,18 @@ fn main() {
     // 自适应类型isize, usize
     println!("max = {}", usize::max_value());
 
+    // 数组
+    // [Type: size]
 
+    let arr: [u32; 5] = [1,2,3,4,5];
+    println!("arr[0]={}", arr[0]);
+    show(arr);
+}
+
+fn show(arr:[u32; 5]){
+    println!("--------------");
+    for i in &arr {
+        println!("{}", i);
+    }
+    println!("--------------")
 }
