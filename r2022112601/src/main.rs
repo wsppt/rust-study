6d2f2d4505a6e7f9b6192cd2e0eb0ffa7606311a
nmapcoco <zmilk2013@126.com>
2022-11-26 22:06:51 +0800

    add

--- r2022112601/src/main.rs
+++ r2022112601/src/main.rs
@@ -1,8 +1,12 @@
+use std::convert::TryInto;
+
 fn main() {
     let a:i32 = 10;
     let b:u16 = 100;
 
-    if a < (b as i32) {  // 小转化大的类型是安全的
-        println!("Ten is less than one hundred.")
+    let b_ = b.try_into().unwrap();
+
+    if a < b_ {
+        println!("Ten is less than one hundred.");
     }
 }
