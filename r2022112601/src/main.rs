1063793fb55df36e761e9471fe691a47e25172ad
nmapcoco <zmilk2013@126.com>
2022-11-26 21:57:29 +0800

    add

--- r2022112601/src/main.rs
+++ r2022112601/src/main.rs
@@ -1,12 +1,8 @@
-use std::rc::Rc;
-use std::sync::{Arc,  Mutex};
-
 fn main() {
-    let a = 10;
-    let b = Box::new(20);
+    let a:i32 = 10;
+    let b:u16 = 100;
 
-    let c = Rc::new(Box::new(30));
-    let d = Arc::new(Mutex::new(40));
-
-    println!("a: {:?}, b: {:?}, c: {:?}, d: {:?}", a, b, c, d);
+    if a < (b as i32) {  // 小转化大的类型是安全的
+        println!("Ten is less than one hundred.")
+    }
 }
