dcfd59717933de5b10ce80df495dc0010a0888b6
nmapcoco <zmilk2013@126.com>
2022-11-26 21:19:42 +0800

    add

--- r2022112601/src/main.rs
+++ r2022112601/src/main.rs
@@ -1,10 +1,6 @@
-use std::thread;
-
 fn main() {
-    let mut data = 100;
-    // 线程的操作由操作系统决定，而不是由应用程序决定
-    thread::spawn(|| {data = 500; });
-    thread::spawn(|| {data = 1000;});
-
-    println!("{}", data);
+    let a = 10;
+    if a == 10 {
+        println!("{}", a);
+    }
 }
