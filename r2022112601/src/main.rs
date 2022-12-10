3e52c486f9e7098c0e04da35174fa6a73be74fbd
nmapcoco <zmilk2013@126.com>
2022-12-10 21:46:37 +0800

    add

--- r2022112601/src/main.rs
+++ r2022112601/src/main.rs
@@ -3,9 +3,9 @@
 // cargo install cargo-edit
 // cargo add num
 
-fn print_type_of<T>(_: &T) {
-    println!("{}", std::any::type_name::<T>())
-}
+// fn print_type_of<T>(_: &T) {
+//     println!("{}", std::any::type_name::<T>())
+// }
 
 fn main() {
     // let  a = Complex{ re: (2.1), im: (-1.2), };
@@ -20,19 +20,33 @@ fn main() {
     //     println!("{}", item);
     // }
 
-    let a = [10, 20, 30, 40, 50];
-    for elemnt in a.iter() {
-        println!("The value is: {}", elemnt);
-    }
+    // let a = [10, 20, 30, 40, 50];
+    // for elemnt in a.iter() {
+    //     println!("The value is: {}", elemnt);
+    // }
+    //
+    // for number in (1..4).rev() {
+    //     println!("{}", number);
+    // }
+    //
+    // let mut s = String::from("Hello");
+    // s.push_str(", world!");
+    // println!("{}", s);
+    //
+    // let ss = "dddddd";
+    // print_type_of(&ss);
 
-    for number in (1..4).rev() {
-        println!("{}", number);
-    }
+    // Rust 有两种字符串: String 和 &str
+    // String 是在堆上分配空间、可以增长的字符序列
+    // &str 是 Sting的切片类型
+    // 形如"foo"的字符串字面值都是&str类型的
+    let s = "galaxy";
+    let s2: String = "galaxy".to_string();
+    let s3: String = String::from("galaxy");
+    let s4: &str = &s3;
 
-    let mut s = String::from("Hello");
-    s.push_str(", world!");
     println!("{}", s);
-
-    let ss = "dddddd";
-    print_type_of(&ss);
+    println!("{}", s2);
+    println!("{}", s3);
+    println!("{}", s4);
 }
